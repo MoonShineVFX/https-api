eff5698372caa7d23bd5a88bce6d857674e20058
Elisha Hung <mrhchief@gmail.com>
2020-05-27 20:59:39 +0800

    add creds

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = require('express')();
 var cors = require('cors');
 
 
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use('/', proxy('work.moonshine.tw:9978'));
 
 const PORT = process.env.PORT || 5000;
