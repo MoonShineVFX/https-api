5b70fc2c552c9506ec899b399f894db607af433b
Elisha Hung <mrhchief@gmail.com>
2020-05-27 21:03:16 +0800

    add allowed origin

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var cors = require('cors');
 
 
 app.use(cors({
+    origin: [
+        'http://jo-ding.web.app',
+        'https://jo-ding.web.app',
+        'http://localhost:5000',
+    ],
     credentials: true
 }));
 app.use('/', proxy('work.moonshine.tw:9978'));
